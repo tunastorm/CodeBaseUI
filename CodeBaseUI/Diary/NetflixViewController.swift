a985c52a256461d9653d0dbcdbe0914c5b3c605d
tunastorm <ycw921@naver.com>
2024-06-04 22:46:56 +0900

    ADD: 넷플릭스 추가

---
+++ CodeBaseUI/Diary/NetflixViewController.swift
@@ -0,0 +1,75 @@
+//
+//  NeflixViewController.swift
+//  CodeBaseUI
+//
+//  Created by 유철원 on 6/4/24.
+//
+
+import UIKit
+
+class NeflixViewController: UIViewController {
+    
+    lazy var mainImageView = {
+        let imageView = UIImageView()
+        
+        return imageView
+    }()
+    
+    lazy var firstHotImageview = {
+        let imageView = UIImageView()
+        
+        return imageView
+    }()
+    
+    lazy var coverImageView = {
+        let imageView = UIImageView()
+        
+        return imageView
+    }()
+    
+    lazy var playImageView = {
+        let imageView = UIImageView()
+        
+        return imageView
+    }()
+    
+    lazy var pickImageView = {
+        let imageView = UIImageView()
+        
+        return imageView
+    }()
+    
+    lazy var TopImageView = {
+        let imageView = UIImageView()
+        
+        return imageView
+    }()
+    
+    
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configHierarchy()
+        configLayout()
+        configHierarchy()
+    }
+    
+    func configNavigationBar() {
+        navigationItem.ti
+    }
+    
+    func configHierarchy() {
+        
+    }
+    
+    func configLayout() {
+    
+    }
+    
+    func configUI() {
+        
+    }
+
+
+}
